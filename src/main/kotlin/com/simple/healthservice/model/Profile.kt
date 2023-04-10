7e79e7f025c142e04756e946aaedda2863a7178e
Neha Srivastava <6138253+neha-sri@users.noreply.github.com>
2023-04-10 00:46:31 -0400

    Add some comments

--- src/main/kotlin/com/simple/healthservice/model/Profile.kt
+++ src/main/kotlin/com/simple/healthservice/model/Profile.kt
@@ -4,6 +4,7 @@ import org.springframework.data.annotation.Id
 import org.springframework.data.relational.core.mapping.Table
 import java.time.LocalDateTime
 
+// @Table automatically generates SQL Tables with the class name
 @Table
 data class Profile(
     @Id var id:Long?,
